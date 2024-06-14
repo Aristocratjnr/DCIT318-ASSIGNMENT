0d28aae9ad5d012e12690a655f5cbde7f8afc927
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-14 15:38:30 +0000

    chore: Add examples for inheritance, abstract classes, and interfaces

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Inheritance and Method Overriding
+        Console.WriteLine("Inheritance and Method Overriding:");
+        Animal animal = new Animal();
+        animal.MakeSound();
+
+        Dog dog = new Dog();
+        dog.MakeSound();
+
+        Cat cat = new Cat();
+        cat.MakeSound();
+        Console.WriteLine();
+
+        // Abstract Classes and Methods
+        Console.WriteLine("Abstract Classes and Methods:");
+        Shape circle = new Circle(5);
+        Console.WriteLine("Area of Circle: " + circle.GetArea());
+
+        Shape rectangle = new Rectangle(4, 5);
+        Console.WriteLine("Area of Rectangle: " + rectangle.GetArea());
+        Console.WriteLine();
+
+        // Interfaces
+        Console.WriteLine("Interfaces:");
+        IMovable car = new Car();
+        car.Move();
+
+        IMovable bicycle = new Bicycle();
+        bicycle.Move();
+    }
+}
